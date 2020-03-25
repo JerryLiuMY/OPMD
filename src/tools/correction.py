6e7ee451896b6e5317e704db23f4889ae768b5b5
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2020-03-25 14:17:23 +0000

    updatw

--- src/tools/correction.py
+++ src/tools/correction.py
@@ -1,53 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-d = 1000
-N = 100
 
-
-def gaussian_intensity(wls):
-    mean, std = 500, 2
+def intensity(wls):
+    mean, std = 532, 2
     Is = (1/(std*np.sqrt(2))) * np.exp(-(1/2)*((wls-mean)/std)**2)
+    Is = np.array(Is)
     return Is
 
 
-def kernel(theta, wls):
+def kernel(theta, wls, d, N):
     wls_over_d = wls / d
-    interference_Is = (np.sin(N*np.pi*np.sin(theta)/wls_over_d)/(N*np.sin(np.pi*np.sin(theta)/wls_over_d))+1e-4)**2
-    return interference_Is
+    inter = (np.sin(N*np.pi*np.sin(theta)/wls_over_d)/(N*np.sin(np.pi*np.sin(theta)/wls_over_d))+1e-4)**2
+    return inter
 
 
-def convolution(Is, wls, thetaz):
-    interference_Iz = []
-    for theta in thetaz:
-        interference_I = sum(kernel(theta, wls) * Is)
-        interference_Iz.append(interference_I)
-    interference_Iz = np.array(interference_Iz)
-    return interference_Iz
+def convolution(Is, wls, wlz, d, N):
+    thetaz = np.arcsin(wlz/d)
+    Iz = [sum(kernel(theta, wls, d, N) * Is) for theta in thetaz]
+    Iz = np.array(Iz)
+    return Iz
 
 
-def deconvolution(interference_Iz, wls, thetaz):
-    interference_Izs = []
-    for theta in thetaz:
-        interference_Is = kernel(theta, wls)
-        interference_Izs.append(interference_Is)
-    interference_Izs = np.array(interference_Izs)
-    Is_ = np.linalg.lstsq(interference_Izs, interference_Iz, rcond=None)[0]
+def deconvolution(Iz, wls, wlz, d, N):
+    thetaz = np.arcsin(wlz / d)
+    Izs = [kernel(theta, wls, d, N) for theta in thetaz]
+    Izs = np.array(Izs)
+    Is_ = np.linalg.lstsq(Izs, Iz, rcond=None)[0]
     return Is_
 
 
-if __name__ == '__main__':
-    wls = np.linspace(480, 520, 5000)
-    thetaz = np.linspace(-np.pi / 2, np.pi / 2, 5000)
-    Is = gaussian_intensity(wls)
-    interference_Iz = convolution(Is, wls, thetaz)
-    Is_ = deconvolution(interference_Iz, wls, thetaz)
-
-    fig, axes = plt.subplots(1, 3, figsize=(24, 8))
-    axes[0].plot(wls, Is)
-    axes[0].set_xlim(480, 520)
-    axes[1].plot(np.sin(thetaz) * d, interference_Iz)
-    axes[1].set_xlim(480, 520)
-    axes[2].plot(wls, Is_)
-    axes[2].set_xlim(480, 520)
-    plt.show(fig)
+# if __name__ == '__main__':
+#     wls = np.linspace(480, 520, 5000)
+#     thetaz = np.linspace(-np.pi / 2, np.pi / 2, 5000)
+#     Is = intensity(wls)
+#     interference_Iz = convolution(Is, wls, thetaz)
+#     Is_ = deconvolution(interference_Iz, wls, thetaz)
+#
+#     fig, axes = plt.subplots(1, 3, figsize=(24, 8))
+#     axes[0].plot(wls, Is)
+#     axes[0].set_xlim(480, 520)
+#     axes[1].plot(np.sin(thetaz) * d, interference_Iz)
+#     axes[1].set_xlim(480, 520)
+#     axes[2].plot(wls, Is_)
+#     axes[2].set_xlim(480, 520)
+#     plt.show(fig)
